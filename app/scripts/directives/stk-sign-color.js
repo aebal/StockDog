6cda46186965de7c38ac88365f00e579b38989dc
Admin Ali Ejaz <a-aejaz@cuscalad.com>
2016-10-07 04:52:45 +1100

    Complete Ref

--- app/scripts/directives/stk-sign-color.js
+++ app/scripts/directives/stk-sign-color.js
@@ -1,18 +1,19 @@
 'use strict';
-
-/**
- * @ngdoc directive
- * @name stockDogApp.directive:stkSignColor
- * @description
- * # stkSignColor
- */
 angular.module('stockDogApp')
-  .directive('stkSignColor', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the stkSignColor directive');
-      }
-    };
-  });
+.directive('stkSignColor', function () {
+	return {
+		restrict : 'A',
+		link : function ($scope, $element, $attrs) {
+			// [1] Use $observe to watch expression for changes
+			$attrs.$observe('stkSignColor', function (newVal) {
+				var newSign = parseFloat(newVal);
+				// [2] Set element's style.color value depending on sign
+				if (newSign > 0) {
+					$element[0].style.color = 'Green';
+				} else {
+					$element[0].style.color = 'Red';
+				}
+			});
+		}
+	};
+});
